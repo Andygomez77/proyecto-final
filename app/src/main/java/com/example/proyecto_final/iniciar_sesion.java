4482d85de01ebe57aa2e41defcd7a07ebf8b66fc
SRevEA <thelord200f@gmail.com>
2024-11-18 14:10:24 -0500

    avance settings

--- app/src/main/java/com/example/proyecto_final/iniciar_sesion.java
+++ app/src/main/java/com/example/proyecto_final/iniciar_sesion.java
@@ -2,6 +2,8 @@ package com.example.proyecto_final;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,7 +13,6 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class iniciar_sesion extends AppCompatActivity {
-
     private EditText email, password;
     private Button acceder;
     private TextView crearCuenta;
@@ -37,7 +38,6 @@ public class iniciar_sesion extends AppCompatActivity {
                 editor.putBoolean("isLoggedIn", true);
                 editor.apply();
 
-
                 Intent intent = new Intent(iniciar_sesion.this, FragmentActivity.class);
                 startActivity(intent);
                 finish();
@@ -54,10 +54,40 @@ public class iniciar_sesion extends AppCompatActivity {
     }
 
     private boolean validarCredenciales(String email, String password) {
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
 
-        return true;
+        Cursor cursor = db.query(
+                DatabaseHelper.TABLE_USERS,
+                new String[]{DatabaseHelper.COLUMN_NOMBRE, DatabaseHelper.COLUMN_EMAIL, DatabaseHelper.COLUMN_FECHA_NACIMIENTO},
+                DatabaseHelper.COLUMN_EMAIL + "=? AND " + DatabaseHelper.COLUMN_PASSWORD + "=?",
+                new String[]{email, password},
+                null, null, null
+        );
+
+        if (cursor.moveToFirst()) {
+
+            String nombre = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_NOMBRE));
+            String fechaNacimiento = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_FECHA_NACIMIENTO));
+
+            SharedPreferences sharedPreferences = getSharedPreferences("prefs", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("nombre", nombre);
+            editor.putString("email", email);
+            editor.putString("fechaNacimiento", fechaNacimiento);
+            editor.putBoolean("isLoggedIn", true);
+            editor.apply();
+
+            cursor.close();
+            db.close();
+            return true;
+        } else {
+            cursor.close();
+            db.close();
+            return false;
+        }
     }
-}
 
+}
 
 
